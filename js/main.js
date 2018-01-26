9964704953e94885f49ba103eb5d0fec9790d8bf
Paul Vegan <buurzuum@gmail.com>
2018-01-26 14:38:23 +0000

    minify images and change path

--- js/main.js
+++ js/main.js
@@ -7,42 +7,30 @@ $(function() {
     var year = today.getFullYear();
     return year;
   });
-  // scroll to
+  // scroll to about section
   $("#about-button").click(function() {
     $('html, body').animate({
       scrollTop: $("#about").offset().top
     }, 1000);
   });
-//   //scrollTop
-//   $('.scrollTop').click(function() {
-//     $('html, body').animate({
-//       scrollTop: 0
-//     }, 800);
-//     return false;
-//   });
-// });
-//
-// // scroll top button
-// $(window).scroll(function() {
-// if ($(this).scrollTop() > 100) {
-//   $('.scrollTop').fadeIn();
-// } else {
-//   $('.scrollTop').fadeOut();
-// }
-// });
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('.scrollTop').fadeIn(500);    // Fade in the arrow
+  $("#works-button").click(function() {
+    $('html, body').animate({
+      scrollTop: $('#works').offset().top
+    }, 1000);
+  });
+  // scrollTop function
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+      $('.scrollTop').fadeIn(500); // Fade in the arrow
     } else {
-        $('.scrollTop').fadeOut(500);   // Else fade out the arrow
+      $('.scrollTop').fadeOut(500); // Else fade out the arrow
     }
-});
-$('.scrollTop').click(function() {      // When arrow is clicked
+  });
+  $('.scrollTop').click(function() { // When arrow is clicked
     $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
+      scrollTop: 0 // Scroll to top of body
     }, 500);
-});
+  });
 
-// script ending
+  // script ending
 });
